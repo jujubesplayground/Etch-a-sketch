9b3d9e2d487569ef93c253db8b4301733a59e59d
Julia SUGIER <jsugier@protegys.com>
2019-07-02 17:07:03 +0200

    fixed size grid plus event listener for clicks

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,24 +3,35 @@ function createDrawingBoard(width, height)
     const drawingBoard = document.querySelector("#drawingBoard")
     let i = 0;
     let j = 0;
-    let board = [];
-
+    let tile;
+    console.log(width + " h " + height)
     console.log(drawingBoard)
 
     while (i < height)
     {
-        board[i] = [];
+        j = 0;
+        console.log("i " + i)
         while (j < width)
         {
-            board[i][j] = document.createElement("div")
-            drawingBoard.appendChild(board[i][j])
-            board[i][j].className = "drawingSquare"
-            //board[i][j].textContent ="hi"
-            j++;
+            console.log("j " + j)
+            tile = document.createElement("div")
+            tile.className = "drawingSquare"
+            tile.addEventListener('click', changeSquareColor)
+            drawingBoard.appendChild(tile)
+            j++;   
         }
         i++;
+        
     }
+    //document.getElementsByClassName("drawingSquare").addEventListener(onmouseover, changeSquareColor)
     console.log("done")
+}
+
+
+function changeSquareColor()
+{
+    console.log('couuuouououuououocouuuuuuuuuu')
+    this.className = "changeColor"
 }
 
 createDrawingBoard(16, 16)
